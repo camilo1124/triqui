537bf64ccc87ea9e08af23b41ef162d7b74bf6f2
Camilo <ccardenas@unal.edu.co>
2023-02-12 06:26:56 -0500

    modificaciones pra el agente

--- triqui.py
+++ triqui.py
@@ -7,246 +7,123 @@ ventana = Tk()
 ventana.title('Juego triqui')
 
 #Empieza X
+
+jugador_1 = "ambiente"
+jugador_2 = "agente"
+signo_j1 = "X"
+signo_j2 = "O"
 clicked = True
 contador = 0
+ganador = False
+print("----------------------")
+print("jugador 1: "+jugador_1)
+print("Signo : "+signo_j1)
+print("----------------------")
+print("jugador 2: "+jugador_2)
+print("Signo : "+signo_j2)
+print("----------------------")
 
+#Cambia signo de los jugadores
+def cambio_signo():
+    pass
+
+#Cambio de orden del jugador que inicia
+
+def cambio_orden():
+    pass
 
 #Desactiva todos los botones
 def desactivacion_botones():
 
-    boton_1.config(state = DISABLED)
-    boton_2.config(state = DISABLED)
-    boton_3.config(state = DISABLED)
-    boton_4.config(state = DISABLED)
-    boton_5.config(state = DISABLED)
-    boton_6.config(state = DISABLED)
-    boton_7.config(state = DISABLED)
-    boton_8.config(state = DISABLED)
-    boton_9.config(state = DISABLED)
-
+    for boton in global tablero:
+        boton.config(state = DISABLED)
 
 #Revisa si en el tablero ha ganado un jugador
 
-def victoria():
+def victoria(signo):
     
-    global winner 
-    ganador = False 
+    global ganador  
 
     #Revision para las X's
 
-    if boton_1["text"] == "X" and  boton_2["text"] == "X" and boton_3["text"] == "X":
-        boton_1.config(bg="green")
-        boton_2.config(bg="green")
-        boton_3.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
+    for i in range(3):
+        for j in range(2):
+            if tablero[(i*3)+0]["text"] == signo and tablero[(i*3)+1]["text"] == signo and tablero[(i*3)+2]["text"] == signo:
+                for k in range(3):
+                    tablero[(i*3)+k].config(bg="green")
+                    ganador = True
+                    messagebox.showinfo("Triqui", (signo + " gana"))
+                    desactivacion_botones()
+                    return True
+            if tablero[i]["text"] == signo and tablero[(i+3)]["text"] == signo and tablero[(i+6)]["text"] == signo:
+                    for k in range(i,9,3):
+                    tablero[k].config(bg="green")
+                    ganador = True
+                    messagebox.showinfo("Triqui", (signo + " gana"))
+                    desactivacion_botones()
+                    return True
+    if tablero[0]["text"] == signo and tablero[4]["text"] == signo andtablero[8]["text"] == signo:
+        tablero[0].config(bg="green")
+        tablero[4].config(bg="green")
+        tablero[8].config(bg="green")
+        ganador = True
+        messagebox.showinfo("Triqui", (signo + " gana"))
         desactivacion_botones()
-    elif boton_4["text"] == "X" and  boton_5["text"] == "X" and boton_6["text"] == "X":
-        boton_4.config(bg="green")
-        boton_5.config(bg="green")
-        boton_6.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
+        return True
+    if tablero[2]["text"] == signo and tablero[4]["text"] == signo andtablero[6]["text"] == signo:
+        tablero[2].config(bg="green")
+        tablero[4].config(bg="green")
+        tablero[6].config(bg="green")
+        ganador = True
+        messagebox.showinfo("Triqui", (signo + " gana"))
         desactivacion_botones()
-    elif boton_7["text"] == "X" and  boton_8["text"] == "X" and boton_9["text"] == "X":
-        boton_7.config(bg="green")
-        boton_8.config(bg="green")
-        boton_9.config(bg="green")
+        return True
 
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-    elif boton_1["text"] == "X" and  boton_4["text"] == "X" and boton_7["text"] == "X":
-        boton_1.config(bg="green")
-        boton_4.config(bg="green")
-        boton_7.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-
-    elif boton_2["text"] == "X" and  boton_5["text"] == "X" and boton_8["text"] == "X":
-        boton_2.config(bg="green")
-        boton_5.config(bg="green")
-        boton_8.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-
-    elif boton_3["text"] == "X" and  boton_6["text"] == "X" and boton_9["text"] == "X":
-        boton_3.config(bg="green")
-        boton_6.config(bg="green")
-        boton_9.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-
-    elif boton_1["text"] == "X" and  boton_5["text"] == "X" and boton_9["text"] == "X":
-        boton_1.config(bg="green")
-        boton_5.config(bg="green")
-        boton_9.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-
-    elif boton_7["text"] == "X" and  boton_5["text"] == "X" and boton_3["text"] == "X":
-        boton_7.config(bg="green")
-        boton_5.config(bg="green")
-        boton_3.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "X gana")
-        desactivacion_botones()
-
-    # Revision para las O's
-
-    if boton_1["text"] == "O" and  boton_2["text"] == "O" and boton_3["text"] == "O":
-        boton_1.config(bg="green")
-        boton_2.config(bg="green")
-        boton_3.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-    elif boton_4["text"] == "O" and  boton_5["text"] == "O" and boton_6["text"] == "O":
-        boton_4.config(bg="green")
-        boton_5.config(bg="green")
-        boton_6.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-    elif boton_7["text"] == "O" and  boton_8["text"] == "O" and boton_9["text"] == "O":
-        boton_7.config(bg="green")
-        boton_8.config(bg="green")
-        boton_9.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-    elif boton_1["text"] == "O" and  boton_4["text"] == "O" and boton_7["text"] == "O":
-        boton_1.config(bg="green")
-        boton_4.config(bg="green")
-        boton_7.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-
-    elif boton_2["text"] == "O" and  boton_5["text"] == "O" and boton_8["text"] == "O":
-        boton_2.config(bg="green")
-        boton_5.config(bg="green")
-        boton_8.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-
-    elif boton_3["text"] == "O" and  boton_6["text"] == "O" and boton_9["text"] == "O":
-        boton_3.config(bg="green")
-        boton_6.config(bg="green")
-        boton_9.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-
-    elif boton_1["text"] == "O" and  boton_5["text"] == "O" and boton_9["text"] == "O":
-        boton_1.config(bg="green")
-        boton_5.config(bg="green")
-        boton_9.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
-
-    elif boton_7["text"] == "O" and  boton_5["text"] == "O" and boton_3["text"] == "O":
-        boton_7.config(bg="green")
-        boton_5.config(bg="green")
-        boton_3.config(bg="green")
-
-        ganador = True 
-
-        messagebox.showinfo("Tic Tac Toe", "O gana")
-        desactivacion_botones()
+    return False
 
 #Estimulo? percepcion? generada por el ambiente
 
 def b_click(boton):
     global clicked, contador
 
-    if boton["text"] == " " and clicked == True:
-        boton["text"] = "X"
-        clicked = False
-        contador += 1
-        victoria()
-    elif boton["text"] == " " and clicked == False:
-        boton["text"] = "O"
-        clicked = True
-        contador += 1
-        victoria()
+    if boton["text"] == " ":
+        if jugador_1 == "ambiente": 
+            boton["text"] = signo_j1
+            contador += 1
+        if victoria(signo_j1) == False:
+            eleccion_agente = programa_agente(contador)
+            contador += 1
+            actuador_agente(eleccion_agente)
+
     else:
         messagebox.showerror("triqui", "Casilla previamente seleccionada\n")
 
 
-def reset():
+def reset_botones():
 
     global boton_1,boton_2,boton_3,boton_4,boton_5,boton_6,boton_7,boton_8,boton_9
     global clicked, contador
     clicked = True
-    count = 0
+    contador = 0
 
-    boton_1 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_1))
-    boton_2 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_2))
-    boton_3 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_3))
-    boton_4 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_4))
-    boton_5 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_5))
-    boton_6 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_6))
-    boton_7 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_7))
-    boton_8 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_8))
-    boton_9 = Button(ventana, text = " ",font = ("Helvetica", 20), height = 3, width = 6,
-                     bg = "Silver", command =lambda: b_click(boton_9))
+    global tablero = [None]*9
+
+    for i in range(9):
+        tablero[i] = Button(ventana, text = " ",font =("Helvetica",20),height = 3, width = 6, bg = "Silver", command = lambda: b_click(tablero[i]))
+
 
     #grid o malla
 
-    boton_1.grid(row = 0, column = 0)
-    boton_2.grid(row = 0, column = 1)
-    boton_3.grid(row = 0, column = 2)
+    for i in range(3):
+        for j in range(3):
+            tablero[((i*3)+j)].grid(row = i,column = j)
 
-    boton_4.grid(row = 1, column = 0)
-    boton_5.grid(row = 1, column = 1)
-    boton_6.grid(row = 1, column = 2)
+    if jugador_1 == "agente":
+        eleccion_agente = programa_agente(contador)
+        contador += 1
+        actuador_agente(eleccion_agente)
+        
 
-    boton_7.grid(row = 2, column = 0)
-    boton_8.grid(row = 2, column = 1)
-    boton_9.grid(row = 2, column = 2)
 
 #Menu
 
@@ -259,9 +136,9 @@ ventana.config(menu = menu_principal)
 
 opciones_menu  = Menu(menu_principal,tearoff = False)
 menu_principal.add_cascade(label = "Opciones", menu = opciones_menu)
-opciones_menu.add_command(label="Reset", command = reset)
+opciones_menu.add_command(label="Reset", command = reset_botones)
 
 
-reset()
+reset_botones()
 ventana.mainloop()
 print("Programa ejecutado")
